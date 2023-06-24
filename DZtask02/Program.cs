165c68af6059cd1a5dbb80fefa803229dddf3d79
MIshka Sinichkin <mishka.jey@mail.ru>
2023-06-24 17:49:19 +0300

    первая домашка на проверку

--- DZtask02/Program.cs
+++ DZtask02/Program.cs
@@ -4,7 +4,7 @@ a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 */
 
 Console.WriteLine("Введите число №1");
-int a= Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число №2");
 int b = Convert.ToInt32(Console.ReadLine());
 int max = 0;
