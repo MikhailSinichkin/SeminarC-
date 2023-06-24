9b7f95ed23e869469b7e2ee4a6519729b058f2ba
MIshka Sinichkin <mishka.jey@mail.ru>
2023-06-24 20:26:30 +0300

    Крайняя классная

---
+++ Task04_/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу , которая принимает на вход трехзначное число
+и на выходе показывает последнюю цифру этого числа.
+456-> 6
+782-> 2
+918-> 8
+*/
+
+Console.WriteLine("Введите трезначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int integer = 0;
+if (number < 100 || number > 999) Console.WriteLine("трезначное, дундук");
+else
+{ 
+    integer = number % 10 ;
+Console.WriteLine(integer);
+}
