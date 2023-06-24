4a0edd0a497f321bd7c40948cba80dc03b65f774
MIshka Sinichkin <mishka.jey@mail.ru>
2023-06-24 18:07:38 +0300

    элсо не получилось

---
+++ Task01_проверка_на_квадрат_числа/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа
+и проверяет , является ли первое число квадратом второго
+a = 25 ; b = 5 -> yes
+a = 2 ; b = 10 -> no
+a = 9 ; b = 3 -> yes
+a = -3 ; b = 9 -> no
+*/
+
+Console.WriteLine("Введите число №1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число №2");
+bool result;
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 == number2 * number2) result = true ;
+
+else result = false;
+ Console.WriteLine(result);
