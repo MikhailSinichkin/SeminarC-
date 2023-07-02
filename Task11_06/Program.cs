a753ab837ae8db96e161ecdaa7cf4f3cce999a9e
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-02 18:47:20 +0300

    task11

---
+++ Task11_06/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Напишите программу , которая выводит трехзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+int numberRundom = new Random().Next(100, 1000);
+
+
+int numberTwo = numberRundom % 10;
+int numberThree = numberRundom / 100;
+
+int result = numberThree*10 + numberTwo;
+Console.WriteLine($"РАндомное число - {numberRundom}");
+Console.WriteLine($" Наше решение - {result}");
