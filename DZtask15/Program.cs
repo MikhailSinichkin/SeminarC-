4603dea9f7ae8b1b109fbdd8c21d34363e55a098
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-03 01:20:28 +0300

    DZ15

---
+++ DZtask15/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("Введите цифру ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+start:
+if (num > 7 || num <1) 
+{
+    Console.WriteLine($"{num} Выбери день меньше 8");
+    num = Convert.ToInt32(Console.ReadLine());
+    goto start;
+}
+if (num > 5)
+{
+    Console.WriteLine("Да - Выходной");
+}
+else if (num < 6)
+{
+    Console.WriteLine("Нет - Будний");
+}
