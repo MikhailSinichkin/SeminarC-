7380c5ab0ba5ee4c62856e37a44a795970854765
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-03 02:04:30 +0300

    task22

---
+++ Task22_13/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу , которая принимает на вход число (N)
+и выдаеттаблицу квадратов чисел от 1 до N.
+*/
+
+Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int mult = 0;
+if (Num > 0)
+{
+    while (count < Num + 1)
+    {
+        mult = count * count;
+        count = count + 1;
+        Console.WriteLine($"{mult} ,");
+    }
+
+}
+else
+{
+    count = count * (-1);
+    while (count >  Num - 1)
+    {
+        mult = count * count;
+        count = count - 1;
+        Console.WriteLine($"{mult} ,");
+    }
+}
