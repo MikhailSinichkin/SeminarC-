e619efeb5c3c58c21713ca2fe8b5407a727fc5a7
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-02 18:10:49 +0300

    sem2

---
+++ Task05_chislonaotrezke/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напииште программу , которая выводит случайное число из
+отрезка [10,99] и показывает наибольшую цифру числа.
+например 
+78->8
+12->2
+85->8
+*/
+
+int rundomNumber = new Random().Next(10,100);
+Console.WriteLine($"РАндомное число - {rundomNumber}");
+
+int numberOne = rundomNumber % 10;
+int numberTwo = rundomNumber / 10;
+Console.Write($" У числа  - {rundomNumber} большая цифра ");
+
+if (numberOne > numberTwo)
+{
+    Console.Write(numberOne);
+}
+else
+{
+    Console.Write(numberTwo);
+}
