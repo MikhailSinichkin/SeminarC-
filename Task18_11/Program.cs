4acc2b1cfb60288602485a6fd37a15581397267b
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-02 21:15:55 +0300

    Task18

---
+++ Task18_11/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Напишите программу , которая по заданному номеру четверти,
+показывает диапозон вводимых координат в точках в этой четверти ( Х и У).
+*/
+
+ Console.WriteLine("Введи номер четверти ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void GetQuarter(int quarter) 
+{
+
+if (quarter == 1)
+{
+    Console.WriteLine($"X and Y > 0");
+
+}
+else if (quarter == 2)
+{
+    Console.WriteLine($" X < 0 and Y > 0");
+
+}
+else if (quarter == 3)
+{
+    Console.WriteLine($" X < 0 and Y < 0");
+
+}
+else if (quarter == 2)
+{
+    Console.WriteLine($" X > 0 and Y < 0");
+
+}
+}
+ GetQuarter(number);
