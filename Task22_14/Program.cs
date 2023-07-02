3d3fc99de58611bda3073917a1c09c0f94ccc861
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-03 02:24:22 +0300

    task2214

---
+++ Task22_14/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Напишите программу , которая принимает на вход число (N)
+и выдаеттаблицу квадратов чисел от 1 до N.
+*/
+
+string Squares(int n)
+{
+    string Squares = "1";
+    if (n > 2)
+    {
+        for (int i = 2; i <= n; i++)
+        {
+            Squares = $"{Squares} {i * i}";
+        }
+    }
+    else
+    {
+        for (int i = 0; i >= n; i--)
+        {
+            Squares = $"{Squares} {i * i}";
+        }
+    }
+    return Squares;
+}
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Squares(number));
