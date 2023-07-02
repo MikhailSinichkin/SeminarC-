34c9cce20b6d1e10c28d611d0c8f0ff7169715b5
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-02 21:37:00 +0300

    Task20

---
+++ Task20_12/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Нужно принять на вход координаты двух точек и посчитать расстояние от них.
+*/
+Console.WriteLine("Enter x for first point");
+int aX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter x for first point");
+int aY = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter x for second point");
+int bX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter x for second point");
+int bY = Convert.ToInt32(Console.ReadLine());
+
+int a = aY -bY;
+int b = aX - bX;
+
+double distance = Math.Sqrt(a*a + b*b);
+Console.WriteLine(distance);
