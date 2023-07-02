cd69e06237d948c23d6f79f6ee3c1834f6525445
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-03 00:18:51 +0300

    TASKDZ10

---
+++ DZtask10/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+start:
+if (num < 100 || num > 999) 
+{
+    Console.WriteLine($"{num} - это не трехзначное число!");
+    num = Convert.ToInt32(Console.ReadLine());
+    goto start;
+}
+
+int result = num / 10 % 10 ;
+Console.WriteLine($"вторая цифра в числе - {result}");
