75b580b5cffbf725749abc77a5aeae101209eabb
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-06 18:59:39 +0300

    DZt29

---
+++ DZtask29/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int [] getRandomArray(int length, int deviation)
+{
+    int [] result = new int [length];
+    for(int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(-deviation, deviation + 1);
+    }
+    return result;
+}
+
+void printArray (int[] arrayToPrint)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("[");
+    for(int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write(arrayToPrint[i]);
+        if (i != arrayToPrint.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+Console.WriteLine("]");
+    Console.ForegroundColor = ConsoleColor.White;
+}
+
+
+int [] randomArray = getRandomArray(8, 9);
+printArray(randomArray);
