8eee5d96cac1e881be747aa77c0f138619aa9f1c
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-06 17:19:10 +0300

    dztask25

---
+++ DZtask25/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход
+ два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+Console.WriteLine("Введите число А");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+int mult = 1;
+
+while (count <= B )
+{
+    mult = mult * A;
+    count += 1;
+    
+}
+
+Console.WriteLine($"{mult} ");
