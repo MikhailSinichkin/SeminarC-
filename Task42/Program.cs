96d83f660bc7ac8602283e0aa42a12f8c150cbb1
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-10 14:27:13 +0300

    42

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*42. Напишите программу, которая будет преобразовывать десятичное число в 
+двоичное.
+
+Валерия Geek Brains 14:05
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+
+Console.WriteLine("Введите число №1");
+int dicimal = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+int binary = 0;
+while (dicimal > 0)
+{
+    binary = binary + dicimal % 2 * count;
+    count = count * 10;
+    dicimal = dicimal / 2;
+}
+
+Console.WriteLine(binary);
