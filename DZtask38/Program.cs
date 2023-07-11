51014aa9dcbe013e75d886e9dc742d0bceafe65c
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-11 03:48:21 +0300

    dztask38

--- DZtask38/Program.cs
+++ DZtask38/Program.cs
@@ -6,10 +6,13 @@
 double[] getRandomDoubleArray(int length)
 {
     double[] result = new double[length];
+    double minValue = 0.0;
+    double maxValue = 100;
     for (int i = 0; i < length; i++)
     {
-        result[i] = new Random().NextDouble();
-        
+        result[i] = new Random().NextDouble() * (maxValue - minValue) + minValue;
+        result[i] = Math.Round(result[i], 2);
+
     }
     return result;
 }
@@ -20,7 +23,7 @@ void printArray(double[] arrayToPrint)
     for (int i = 0; i < arrayToPrint.Length; i++)
     {
         Console.Write(arrayToPrint[i]);
-        if (i != arrayToPrint.Length-1)
+        if (i != arrayToPrint.Length - 1)
         {
             Console.Write(" , ");
         }
@@ -28,5 +31,32 @@ void printArray(double[] arrayToPrint)
     Console.WriteLine("]");
 }
 
-double [] randomArray = getRandomDoubleArray(6);
+double FindMax(double[] array)
+{
+    double max = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    return max;
+
+}
+double FindMin(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+double[] randomArray = getRandomDoubleArray(6);
 printArray(randomArray);
+Console.WriteLine($"максимум-{FindMax(randomArray)}");
+Console.WriteLine($"минимум-{FindMin(randomArray)}");
+Console.WriteLine($"разница-{FindMax(randomArray) - FindMin(randomArray)}");
