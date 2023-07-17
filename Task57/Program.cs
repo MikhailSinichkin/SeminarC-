30e3e5f541c077dd4bed2b1182995c911a7d8ce6
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-17 18:35:18 +0300

    task57

---
+++ Task57/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+57. Составить частотный словарь элементов двумерного массива.
+Частотный словарь содержит информацию о том,
+ сколько раз встречается элемент входных данных
+{ 1, 9, 9, 0, 2, 8, 0, 9 }
+0 встречается 2 раза
+1 встречается 1 раз
+2 встречается 1 раз
+8 встречается 1 раз
+9 встречается 3 раза
+
+1, 2, 3
+4, 6, 1
+2, 1, 6
+1 встречается 3 раза
+2 встречается 2 раза
+3 встречается 1 раз
+4 встречается 1 раз
+6 встречается 2 раза
+*/
+
+int[,] GetRandom2DArray(int rowNumber, int colNumber, int deviation)
+{
+    int[,] result = new int[rowNumber, colNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            result[i, j] = new Random().Next(-deviation, deviation + 1);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] RowsToColumns(int[,] array)
+{
+    int[,] matrix = new int[array.GetLength(0), array.GetLength(1)];
+    if (array.GetLength(0) != array.GetLength(1))
+    {
+    return matrix;
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            matrix [j, i] = array [i, j];
+        }
+    }return matrix;
+}
+int[,] matrix = GetRandom2DArray(4, 5, 10);
+Print2DArray(matrix);
+System.Console.WriteLine();
+int [,] newMatrix = RowsToColumns(matrix);
+Print2DArray(newMatrix);
