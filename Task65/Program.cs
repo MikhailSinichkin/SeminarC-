8ff730bf8c12b0ba43ec1e93b8deed18874657e0
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-26 18:04:37 +0300

    рекурсия еще легче

---
+++ Task65/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Задайте значение N. Напишите программу ,
+которая выведет все натуральные числа в промежутке от M до N.
+*/
+
+void NaturaleNumbers(int number1, int number2 )
+{
+    if (number1 <= number2)
+    {
+        Console.Write(number1+ " " );
+        NaturaleNumbers(number1+1,number2);
+    }
+}
+NaturaleNumbers(4,8);
