bb1d13d918a6b2c96e3f40e8aa26d2a712f439a3
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-26 17:44:41 +0300

    первая рекурсия

---
+++ Task63/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задайте значение N. Напишите программу ,
+ которая выдает все натуральные чтсла в промежутке от 1 да N.
+*/
+
+void NaturaleNumbers(int number, int count = 1)
+{
+    if (number < count)
+
+    return;
+    else
+    {
+        Console.Write(count+ " " );
+        NaturaleNumbers(number,count+1);
+    }
+}
+NaturaleNumbers(5);
