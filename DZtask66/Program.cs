024e478fef6f42d9fdf447b97fa84e47143d6228
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-26 21:26:57 +0300

    Сумма чисел от до N

---
+++ DZtask66/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму 
+натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int recSum(int m, int n)
+{
+    if (m >= n)
+    {
+        return m;
+    }
+    else
+    {
+        return recSum(m + 1, n) + m;
+    }
+}
+
+System.Console.WriteLine(recSum(1, 15));
