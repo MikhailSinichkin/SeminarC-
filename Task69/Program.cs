2080432233d7f2ab4c948c517e350435f4006f9e
MIshka Sinichkin <mishka.jey@mail.ru>
2023-07-26 20:03:42 +0300

    task69

---
+++ Task69/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу , которая на вход принимает А и В ,
+и возводит число А в целую степень В с помощью рекурсии.
+*/
+
+int ATiPowB(int a, int b)
+{
+    if (b == 1)
+    {
+        return a;
+    }
+    else
+    {
+        return a * ATiPowB(a, b - 1);
+    }
+}
+System.Console.WriteLine(ATiPowB(2, 9));
